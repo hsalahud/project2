13dbf75b3ea07c0e15069110e99f810c13b52548
hamzasalah <hamzasalah@gmail.com>
2019-05-22 19:27:56 -0700

    added react and modified package.json for react

---
+++ config/index.js
@@ -0,0 +1,8 @@
+//localStorage.getItem('keyname')
+//document.querySelectio('').file
+//refer to 50min mark of https://codingbootcamp.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=8fd69687-c7e0-4dec-9aa0-aa1c0019337a
+
+//get user info:
+//firebase.auth().onAuthStateChanged(user => {
+  // console.log(user)
+// })
