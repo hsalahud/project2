a6962fe4405c27908c989f6d32c0a41232ae45de
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-05-24 20:23:07 -0700

    working on navbar

---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import PieChartIcon from '@material-ui/icons/PieChart'
+import PersonIcon from '@material-ui/icons/Person'
+import MoodIcon from '@material-ui/icons/Mood'
+import ChatIcon from '@material-ui/icons/Chat'
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0
+  }
+})
+
+class NavBar extends Component () {
+  render () {
+  const classes = useStyles()
+  const [value, setValue] = React.useState('recents')
+
+  function handleChange (event, newValue) {
+    setValue(newValue)
+  }
+
+  return (
+    <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+      <BottomNavigationAction label='Stats' value='stats' icon={<PieChartIcon />} />
+      <BottomNavigationAction label='Profile' value='profile' icon={<PersonIcon />} />
+      <BottomNavigationAction label='Matches' value='matches' icon={<MoodIcon />} />
+      <BottomNavigationAction label='Chat' value='chat' icon={<ChatIcon />} />
+    </BottomNavigation>
+  )
+}
+
+export default NavBar
--- routes/user.js
+++ routes/user.js
@@ -4,22 +4,26 @@ module.exports = app => {
   // your user routes go here...
   // GET all users
   app.get('/user', (req, res) => {
-
-  })
-  // GET a user
-  app.get('/user/:id', (req, res) => {
-
+    User.findAll()
+      .then(user => res.json(user))
+      .catch(e => console.log(e))
   })
   // POST a user
-  app.post('/user', (req, res) => {
-
+  app.post('/user/bulk', (req, res) => {
+    User.create(req.body)
+      .then(_ => res.sendStatus(200))
+      .catch(e => console.log(e))
   })
   // PUT a user
   app.put('/user/:id', (req, res) => {
-
+    User.update(req.body, { where: { id: req.params.id } })
+      .then(_ => res.sendStatus(200))
+      .catch(e => console.log(e))
   })
   // DELETE a user
   app.delete('/user/:id', (req, res) => {
-
+    User.destroy({ where: { id: req.params.id } })
+      .then(_ => res.sendStatus(200))
+      .catch(e => console.log(e))
   })
 }
