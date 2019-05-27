b14b4173c54bce6a599e8d65b647954ac98cda9d
hamzasalah <hamzasalah@gmail.com>
2019-05-27 12:47:58 -0700

    adjusted models and added login component. Removed force true from server

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ require('./routes')(app)
 
 // require('./data')
 
-require('./config').sync({force:true})
+require('./config').sync()
   .then(_ => app.listen(process.env.PORT || 3001))
   .catch(e => console.log(e))
