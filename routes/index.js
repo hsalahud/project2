4d19c9bedf8b1aafe6dbea6eb9ee999bb83c4dd2
hamzasalah <hamzasalah@gmail.com>
2019-06-03 20:45:35 -0700

    pointless change

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,5 @@ module.exports = app => {
   require('./timeLog.js')(app)
   require('./imageRoutes.js')(app)
   require('./userRoutes.js')(app)
+  app.get('/isTeapot', (req, res) => res.send('no'))
 }
